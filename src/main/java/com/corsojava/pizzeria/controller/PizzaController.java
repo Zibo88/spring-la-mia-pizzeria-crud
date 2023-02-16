57c0c810f59ac095e9524cebc1171c0c35015480
Zibo88 <vincenzo.tardino1988@gmail.com>
2023-02-16 15:33:26 +0100

    la mia pizzeria parte create

--- src/main/java/com/corsojava/pizzeria/controller/PizzaController.java
+++ src/main/java/com/corsojava/pizzeria/controller/PizzaController.java
@@ -6,13 +6,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.corsojava.pizzeria.model.Pizza;
 import com.corsojava.pizzeria.repository.PizzaRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/pizza")
@@ -35,10 +40,29 @@ public class PizzaController {
 	}
 	
 	@GetMapping("/{id}")
-	public String dettaglioPizza(@PathVariable("id") Integer id, Model model) {
+	public String show(@PathVariable("id") Integer id, Model model) {
 		Pizza  singolaPizza = pizzaRepository.getReferenceById(id);
 		model.addAttribute("pizza",singolaPizza);
 		return "pizzaDetail";
+	}
+	
+	@GetMapping("/create")
+	public String create(Model model) {
+		Pizza newPizza = new Pizza();
+		model.addAttribute("pizza" ,newPizza);
+		return "create";
+	}
+	
+	@PostMapping("/create")
+	public String strore(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult ,Model model) {
+		
+		if(bindingResult.hasErrors()) {
+			return "/create";
+		}
+		
+		pizzaRepository.save(formPizza);
+		
+		return "redirect:/pizza";
 	}
 	
 	
